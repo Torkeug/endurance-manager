7ba6867362d8dbdd07accd81cf446569b21539a5
Raphaël Laurent <le_queteur@hotmail.com>
2026-04-11 17:01:38 +0200

    fix iRacing callback import path

--- .history/app/auth/callback/iracing/route_20260411170103.js
+++ .history/app/auth/callback/iracing/route_20260411170103.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '../../../../lib/auth'
+import { createClient } from '../../../../../../lib/auth'
 
 // iRacing OAuth configuration
 const IRACING_TOKEN_URL = 'https://members-ng.iracing.com/oauth2/token'
